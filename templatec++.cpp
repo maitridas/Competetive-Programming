d76f42d60435f7cbe10db2f2a3ca0e6ecc979285
Maitri Das <36633371+maitridas@users.noreply.github.com>
2020-07-15 02:29:52 +0530

    Add files via upload

---
+++ templatec++.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <iomanip>
+#include <algorithm>
+#include <functional>
+#include <stdio.h>
+#include <cmath>
+#include <queue>
+#include <string>
+#include <map>
+#include <complex>
+#include <stack>
+#include <set>
+ 
+#define FOR(i,n) for(int i=0;i<n;i++)
+#define FORE(i,a,b) for(int i=a;i<=b;i++)
+#define ll long long int
+#define vi vector<int>
+#define ii pair<int,int>
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define pll pair<ll,ll>
+#define cd complex<double> 
+#define vv vector
+using namespace std;
+
+int main()
+{
+
+}
